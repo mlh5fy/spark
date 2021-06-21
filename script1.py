9b48172f20e7b20b44552bc79f468e41e4e32c5d
Maddie Hennessy <maddiehennessy@Maddies-MacBook-Pro.local>
2021-06-21 10:34:38 -0400

    added script1

---
+++ script1.py
@@ -0,0 +1,11 @@
+# File: pyScript01-review.py
+# DS 5100
+# Learning Python (Python version: 3)
+
+# Simplest (complete) Python program: "Hello World"
+# Use print() function
+print("Hello, World!")
+
+# Note: Any line that begins with "#" is a comment. Such lines are not run/compiled.
+# Therefore, this "Hello World" program consists of a single line of Python code!  
+# Everything else in this file is a comment (for your benefit)! 
